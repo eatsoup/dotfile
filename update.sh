1b14e3ddba13d1799174e88b5bf5726281f3ddb9
eatsoup <eatsoup@gmail.com>
2019-03-13 15:09:11 +0100

    instant update bash

--- update.sh
+++ update.sh
@@ -3,13 +3,17 @@
 if [ $EUID -eq 0 ]
 then
     echo 'Root detected'
+    echo 'Please run as normal user'
     exit 1
 fi
 
-# Remove old bash and vimrc
+# Update bashrc, vimrc and update.sh
 rm ~/.bashrc -f
 rm ~/.vimrc -f
 curl -o ~/.vimrc https://raw.githubusercontent.com/eatsoup/dotfile/server/.vimrc
 curl -o ~/.bashrc https://raw.githubusercontent.com/eatsoup/dotfile/server/.bashrc
 curl -o ~/update.sh https://raw.githubusercontent.com/eatsoup/dotfile/server/update.sh
 chmod +x ~/update.sh
+
+# We want it NOW!
+source ~/.bashrc
